1ac0790f035900ca280522fd5139df73f3fb5eed
Moni361989 <monica.ocampom@perficient.com>
2023-09-27 12:03:22 -0500

    Funciones

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  beforeAll("passes", () => {
+    cy.visit("http://localhost:4200/public/login");
+  });
+});
+
+/*describe("template spec", () => {
+   
+  });
+
+function sumVal (num1, num2, additionalAction) {
+  additionalAction();
+  return num1 + num2;
+};
+
+sumVal(13,27,() =>{
+  console.log('Hola');
+});*/
